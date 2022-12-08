47a4a733e601d43eb9835a7bf3b5be454300551a
Sanara Felicio <sanara@cpqd.com.br>
2022-12-08 11:19:48 -0300

    criado cenário de teste para usuário não cadastrado

--- src/test/java/br/com/sanara/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/sanara/leilao/dao/UsuarioDaoTest.java
@@ -1,6 +1,7 @@
 package br.com.sanara.leilao.dao;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
@@ -13,20 +14,35 @@ class UsuarioDaoTest {
 	private UsuarioDao dao;
 
 	@Test
-	void testBuscaUsuarioPeloUsername() {
+	void testDeveriaEncontrarUsuarioCadastradoPeloUsername() {
 		EntityManager em = JPAUTil.getEntityManager();
 		this.dao = new UsuarioDao(em);
 
 		Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
-		//inicia a transação
+		// inicia a transação
 		em.getTransaction().begin();
-		//faz a persistencia do novo usuário
+		// faz a persistencia do novo usuário
 		em.persist(usuario);
-		//faz o commit dos dados
+		// faz o commit dos dados
 		em.getTransaction().commit();
 
 		Usuario usuarioEncontrado = this.dao.buscarPorUsername(usuario.getNome());
 		Assert.assertNotNull(usuarioEncontrado);
+
+	}
+
+	
+	@Test
+	void testNaoDeveriaEncontrarUsuarioCadastradoPeloUsername() {
+		EntityManager em = JPAUTil.getEntityManager();
+		this.dao = new UsuarioDao(em);
+
+		Usuario usuario = new Usuario("ciclano", "ciclano@email.com", "12345678");
+		em.getTransaction().begin();
+		em.persist(usuario);
+		em.getTransaction().commit();
+
+		Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("beltrano"));
 
 	}
 
