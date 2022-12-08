bb49646d01cfef9917ece25e2063a18d9a02da8b
Sanara Felicio <sanara@cpqd.com.br>
2022-12-08 14:37:29 -0300

    refatoração de código de teste

--- src/test/java/br/com/sanara/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/sanara/leilao/dao/UsuarioDaoTest.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.sanara.leilao.model.Usuario;
@@ -13,37 +15,46 @@ class UsuarioDaoTest {
 
 	private UsuarioDao dao;
 
-	@Test
-	void testDeveriaEncontrarUsuarioCadastradoPeloUsername() {
-		EntityManager em = JPAUTil.getEntityManager();
-		this.dao = new UsuarioDao(em);
+	private EntityManager em;
 
-		Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
+	// recurso do junit para inicializar antes de cada um dos testes inicializar o  em
+	@BeforeEach
+	public void beforeEach() {
+		this.em = JPAUTil.getEntityManager();
+		this.dao = new UsuarioDao(em);
 		// inicia a transação
 		em.getTransaction().begin();
-		// faz a persistencia do novo usuário
-		em.persist(usuario);
-		// faz o commit dos dados
-		em.getTransaction().commit();
-
-		Usuario usuarioEncontrado = this.dao.buscarPorUsername(usuario.getNome());
-		Assert.assertNotNull(usuarioEncontrado);
-
 	}
 
-	
+	// Chama o método após a execução de cada teste
+	@AfterEach
+	public void afterEach() {
+		// faz o rollback dos dados no banco zerando para o proximo teste
+		this.em.getTransaction().rollback();
+	}
+
+	private Usuario criarUsuario() {
+		Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
+		// faz a persistencia do novo usuário
+		this.em.persist(usuario);
+		return usuario;
+	}
+
+	@Test
+	void testDeveriaEncontrarUsuarioCadastradoPeloUsername() {
+
+		// faz o commit dos dados
+		// em.getTransaction().commit();
+
+		Usuario usuario = criarUsuario();
+		Usuario usuarioEncontrado = this.dao.buscarPorUsername(usuario.getNome());
+		Assert.assertNotNull(usuarioEncontrado);
+	}
+
 	@Test
 	void testNaoDeveriaEncontrarUsuarioCadastradoPeloUsername() {
-		EntityManager em = JPAUTil.getEntityManager();
-		this.dao = new UsuarioDao(em);
-
-		Usuario usuario = new Usuario("ciclano", "ciclano@email.com", "12345678");
-		em.getTransaction().begin();
-		em.persist(usuario);
-		em.getTransaction().commit();
-
+		criarUsuario();
 		Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("beltrano"));
-
 	}
 
 }
